db4cf25e34951a84d3c238b3ef4e21a88764d4dc
HaiYanHUHU <x22247327@student.ncirl.ie>
2024-03-30 16:36:05 +0000

    Create Adder.java
    renewal

---
+++ Adder.java
@@ -0,0 +1,19 @@
+
+
+
+// Adder ad = new Adder();
+// try{
+// 	ad.add(10,2)
+// 	int x = 20/0;
+// 	System.out.print(ad.getSum() + x);	
+// }
+// catch(ArithmenticException e){
+// 	System.out.print(2);
+// }
+// catch(Exception e){
+// 	System.out.print(1);
+// }
+// finally{
+// 	System.out.print(3);
+// }
+
